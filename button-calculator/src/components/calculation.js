110f6960d556d6fb371d6d6d59c995fc182e1a7e
Michael <mlarock711@comcast.net>
2021-06-09 18:22:02 -0400

    constant for a second input and operands put into front end.

--- button-calculator/src/components/calculation.js
+++ button-calculator/src/components/calculation.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 
 const Calculation = () => {
-  const [firstInput, setFirstInput] = useState ('')
-  
-  
+  const [firstInput, setFirstInput] = useState('')
+  const [secondInput, setSecondInput] = useState('')
+  const [operator, setOperator] = useState('')
+
   const updatValue = (event) => {
     setValue ()
   }
@@ -13,6 +14,13 @@ const Calculation = () => {
       <hi className='title'>React Calcutator</hi>
       <input type='text' name='firstInput' value={firstInput} onChange={event =>
       setFirstInput(event.target.value)} />
+      <select name='operator' value={operator} onChange={event => 
+      setOperator(event.target.value)} >
+        <option value='+'>+</option>
+        <option value='-'>-</option>
+        <option value='/'>/</option>
+        <option value='*'>*</option>
+      </select>
     </div>
 
   )
