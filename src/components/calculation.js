75105ada5b170aae678d29fa72cd1706c37b89e2
Michael <mlarock711@comcast.net>
2021-06-10 00:32:50 -0400

    A swith cases for running the equations has been done.

--- src/components/calculation.js
+++ src/components/calculation.js
@@ -13,13 +13,26 @@ const Calculation = () => {
 
   const calculation = () => {
     if (!validAnswer(firstInput) || !validAnswer(secondInput)) {
-      return setError('Please provide a vaild number for both inputs')
+      return setError("Please provide a vaild number for both inputs")
     }
 
     setError('')
     setAnswer('')
 
-    
+    switch (operator) {
+      case 'add':
+        return setAnswer(Number(firstInput) + Number(secondInput))
+      case 'subtract':
+        return setAnswer(Number(firstInput) - Number(secondInput))
+      case 'divide':
+        return setAnswer(Number(firstInput) / Number(secondInput))
+      case 'multiply':
+        return setAnswer(Number(firstInput) * Number(secondInput))
+      default: 
+        return setError('Please give a valid input') 
+  
+    }
+
   }
 
 
@@ -27,22 +40,22 @@ const Calculation = () => {
 
 
   return (
-    <div className='Calculator'>
-      <hi className='title'>React Calcutator</hi>
-      <input type='text' name='firstInput' value={firstInput} onChange={event =>
+    <div className="Calculator">
+      <hi className="title">React Calcutator</hi>
+      <input type="text" name="firstInput" value={firstInput} onChange={event =>
         setFirstInput(event.target.value)} />
-      <select name='operator' value={operator} onChange={event => 
+      <select name="operator" value={operator} onChange={event => 
         setOperator(event.target.value)} >
-        <option value='+'>+</option>
-        <option value='-'>-</option>
-        <option value='/'>/</option>
-        <option value='*'>*</option>
+        <option value="+">+</option>
+        <option value="-">-</option>
+        <option value="/">/</option>
+        <option value="*">*</option>
       </select>
-      <input type='text' name='secondInput' value={secondInput} onChange={event =>
+      <input type="text" name="secondInput" value={secondInput} onChange={event =>
         setSecondInput(event.target.value)} /> 
-      <button type='button' value='=' onClick={calculation} />
-      <input type='text' value={equation} disabled />
-      <div className='error'>{error}</div>
+      <button type="button" value="=" onClick={calculation} />
+      <input type="text" value={answer} disabled />
+      <div className="error">{error}</div>
     </div>
 
   )
